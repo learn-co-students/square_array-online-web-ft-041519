a8399ed518a93ce14f0fedfe2e6307dafe2a5473
Annia Forney <anniaforney@gmail.com>
2019-04-19 20:36:04 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
-  # your code here
+  array.each 
+end 
+
+def square_array(array) do 
+  elements = [0,1,2]
+  square_array(elements)
+  new elements = [3,4,5,6]
+  square_array(new elements)
 end
+
+def square_array(array) 
+  array.collect 
+end 
